de9912a7ade605cd537f023a27fede6784dda0f6
Luiz Thiago <luiz@argus.gg>
2025-09-04 19:33:20 -0400

    Started the fluent mapping

--- Data/BlogDataContext.cs
+++ Data/BlogDataContext.cs
@@ -7,10 +7,8 @@ public class BlogDataContext : DbContext
 {
 	public DbSet<Category> Categories { get; set; }
 	public DbSet<Post> Posts { get; set; }
-	public DbSet<Tag> Tags { get; set; }
 	public DbSet<User> Users { get; set; }
-	public DbSet<Role> Roles { get; set; }
 
 	override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
-		optionsBuilder.UseSqlServer("Server=localhost,1433;Database=Blog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
+		optionsBuilder.UseSqlServer("Server=localhost,1433;Database=FluentBlog;User ID=sa;Password=1q2w3e4r@#$;Trusted_Connection=False; TrustServerCertificate=True;");
 }
