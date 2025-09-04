4f3514c01087cf6be93fa64e0e83c58418c725b1
Luiz Thiago <luiz@argus.gg>
2025-09-04 10:53:13 -0400

    Add basic Data Annotations to entity models for EF Core mapping

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models;
 
+[Table("Post")]
 public class Post
 {
+	[Key]
+	[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 	public int Id { get; set; }
-	public int AuthorId { get; set; }
+
 	public string Title { get; set; }
 	public string Slug { get; set; }
 	public string Summary { get; set; }
 	public string Body { get; set; }
 	public DateTime CreateDate { get; set; }
 	public DateTime LastUpdateDate { get; set; }
+
+	[ForeignKey("CategoryId")]
 	public int CategoryId { get; set; }
+	public Category Category { get; set; } // Navigation Property*
+
+	[ForeignKey("AuthorId")]
+	public int AuthorId { get; set; }
+	public User Author { get; set; } // Navigation Property*
+
+	// * Navigation Property is a property that references another entity in the model.
+	// It allows navigation from one entity to another and access related data.
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog;
 
@@ -8,9 +9,85 @@ class Program
 	static void Main(string[] args)
 	{
 		using var context = new BlogDataContext();
-		
-		var tag = new Tag { Name = "ASP.NET Core", Slug = "aspnet-core" };
-		context.Tags.Add(tag);
+
+		//CreateUserAndPost(context);
+
+		//ListPosts(context);
+
+		//UpdatePost(context);
+	}
+
+	private static void CreateUserAndPost(BlogDataContext context)
+	{
+		var user = new User
+		{
+			Name = "Luiz",
+			Email = "luiz.thiago@gmail.com",
+			PasswordHash = "12345_HASH",
+			Bio = "Programador .NET",
+			Image = "http://luizthiago.com",
+			Slug = "luiz"
+		};
+
+		var category = new Category
+		{
+			Name = "Backend",
+			Slug = "backend"
+		};
+
+		var post = new Post
+		{
+			Author = user,
+			Category = category,
+			Title = "My first post",
+			Slug = "my-first-post",
+			Summary = "This is my first post",
+			Body = "This is the body of my first post",
+			CreateDate = DateTime.Now,
+			LastUpdateDate = DateTime.Now
+		};
+
+		context.Posts.Add(post);
 		context.SaveChanges();
+	}
+
+	private static void ListPosts(BlogDataContext context)
+	{
+		var posts = context
+			.Posts
+			.AsNoTracking()
+			.Include(x => x.Author)
+			.Include(x => x.Category)
+			.OrderBy(x => x.LastUpdateDate)
+			.ToList();
+
+		foreach (var post in posts)
+		{
+			Console.WriteLine($"----- {post.Title} -----");
+			Console.WriteLine($"created: {post.CreateDate} - last updated: {post.LastUpdateDate}");
+			Console.WriteLine($"Author: {post.Author?.Name} - Category: {post.Category?.Name}");
+			Console.WriteLine(post.Summary);
+			Console.WriteLine(post.Body);
+			Console.WriteLine();
+		}
+	}
+
+	private static void UpdatePost(BlogDataContext context)
+	{
+		var post = context
+			.Posts
+			.Include(x => x.Author)
+			.Include(x => x.Category)
+			.OrderBy(x => x.LastUpdateDate)
+			.FirstOrDefault();
+
+		if (post != null)
+		{
+			post.LastUpdateDate = DateTime.Now;
+			post.Author.Name = "Edited Author";
+
+			context.Posts.Update(post);
+			context.SaveChanges();
+		}
 	}
 }
